7b8b2e72be7476f7bff51c0980e3322d18691371
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-22 05:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function setCart(c) {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100) + 1;
-  var added = cart.push({ [item]: price });
-  console.log(`${added} has been added to your cart.`)
+  cart.push({ [item]: price });
+  console.log(`${item} has been added to your cart.`)
   return cart;
 }
 
 function viewCart() {
-  // write your code here
+  // write your code here 
 }
 
 function total() {
